1d45cbec721c062b1f35d87f9fa409dc64b86da6
Javier Piedra <javier.piedra@icloud.com>
2019-04-24 18:56:03 -0500

    adding geolocation

--- findmypet/src/App.js
+++ findmypet/src/App.js
@@ -7,13 +7,13 @@ import Report from './components/Report'
 import Header from './components/Header'
 import { Router, Redirect } from '@reach/router'
 import NotFound from './components/NotFound';
-import getLocation from './components/getLocation'
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <getLocation />
         <Header/>
         <Router>
           <NotFound default/>
