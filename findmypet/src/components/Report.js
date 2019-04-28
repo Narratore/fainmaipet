f5fe2a250f8742b86f71e0a98abfa9f553a9a402
Luis A <zeronarratore@gmail.com>
2019-04-27 19:15:58 -0500

    el perfil de perro

--- findmypet/src/components/Report.js
+++ findmypet/src/components/Report.js
@@ -4,7 +4,6 @@ import { useFetch } from './hooks'
 import { Link } from '@reach/router'
 
 
-<<<<<<< HEAD
 export default function Form() {
 
 
@@ -187,33 +186,6 @@ if (apiResponse === ""){
                     <p>Lo siento<br/></p>
                 </Col>
             </Row>
-=======
-export default function Report(props) {
-
-    const [data, loading] = useFetch(
-        ("https://cryptic-scrubland-58057.herokuapp.com/api/publication/"+props.reportid)+'/'
-      );
-
-    return(
-        <div>
-            { loading ? ("Loading...") : (
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to='/feed'>Regresar</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <h1>
-                        Este es el reporte de {data.title}
-                    </h1>
-                    <p>
-                        Creado el {data.created}
-                    </p>
-                </div>
-            ) }
->>>>>>> master
         </div>
         )
 }
